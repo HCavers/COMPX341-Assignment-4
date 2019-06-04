83e273049463fb5b6d41b7d5e416bfb81ea1a5d3
hc183 <hc183@cms-r1-42.cms.waikato.ac.nz>
2019-06-04 16:04:29 +1200

    Implemented api call to tell whether a number is prime or not

--- AppCode/app.py
+++ AppCode/app.py
@@ -1,7 +1,8 @@
 import time
-
 import redis
 from flask import Flask
+from math import sqrt
+from itertools import count, islice
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
@@ -18,7 +19,16 @@ def get_hit_count():
             retries -= 1
             time.sleep(0.5)
 
+def check_prime(num):
+    n = int(num,10)
+    if n  < 2:
+        return False
 
+    for number in islice(count(2), int(sqrt(n)-1)):
+        if n % number == 0:
+            return False
+
+    return True
 
 @app.route('/hello')
 def hello():
@@ -27,4 +37,7 @@ def hello():
 
 @app.route('/isPrime/<number>')
 def isPrime(number):
-    return '{} is a prime.\n'.format(number)
+    if check_prime(number):
+         return '{} is a prime.\n'.format(number)
+    else:
+         return '{} is not a prime.\n'.format(number)
